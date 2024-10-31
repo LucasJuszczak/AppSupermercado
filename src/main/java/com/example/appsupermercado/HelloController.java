66b4acc94973291d9799f9a11500fb9ac8a20028
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-10-30 22:15:49 -0300

    Create Project AppSupermercado

--- src/main/java/com/example/appsupermercado/HelloController.java
+++ src/main/java/com/example/appsupermercado/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.appsupermercado;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Menu;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    protected void onCaixaButtonClick() throws IOException {
+        new JanelaCaixa();
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onVerificadorPrecoButtonClick() {
     }
+
+    @FXML
+    protected void onSobreButtonClick(){
+    }
+
+    @FXML
+    protected void onSairButtonClick(){
+        System.exit(0);
+    }
+
 }
